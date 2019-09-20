ba5c710c9b0f0592ae037251e647fb5f20ab18f9
jappoker <walkerinvrains@gmail.com>
2019-09-20 15:00:41 -0400

    save 9/19 HW 2

---
+++ HW2_Due0923/HW3_2B.m
@@ -0,0 +1,37 @@
+clearvars; 
+
+% plot initialization
+figure(2);
+clf;
+set(gca,'LineWidth',1,'FontSize',14);
+hold on;
+
+% parameter settings
+l_list = 1:1:10;
+
+meanlist = zeros(10,1);
+varlist = zeros(10,1);
+
+for l=l_list
+    p = @(d) l*exp(-l*d);
+    
+    % mean = E(x) = \int x * p(x) dx
+    
+    p_d = @(d) l*exp(-l*d) .* d;
+    meanlist(l) = integral( p_d,0,Inf);
+    
+    % variance = E(x^2) - E(x)^2
+    %          = \int x^2 * p(x) dx - mean ^2
+    
+    p_d2 = @(d) l*exp(-l*d) .* d .*d;
+    varlist(l) = integral(p_d2,0,Inf) - meanlist(l)^2;
+    
+end
+
+plot(l_list,meanlist,"-o",l_list,varlist,"-o",...
+      'LineWidth',2, 'MarkerSize',10);
+legend('Mean','Variance');
+
+xlabel(sprintf('\\lambda'));
+ylabel('Mean & Variance value');
+
